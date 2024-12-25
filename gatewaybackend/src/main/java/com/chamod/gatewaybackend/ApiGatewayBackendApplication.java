94ce8a741fc292fd50e503abaeb577e024f7cc97
Bruce Wayne <83892557+Chamod07@users.noreply.github.com>
2024-12-25 12:57:58 +0530

    Create Spring Boot project for the Gateway

---
+++ gatewaybackend/src/main/java/com/chamod/gatewaybackend/ApiGatewayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.chamod.gatewaybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayBackendApplication.class, args);
+	}
+
+}
---
+++ gatewaybackend/src/test/java/com/chamod/gatewaybackend/ApiGatewayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chamod.gatewaybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
